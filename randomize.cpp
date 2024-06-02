e599c696f5297e96e7b3ea81d983e46f10c6cc73
peepeds <edbertmac@gmail.com>
2024-06-02 14:14:10 +0700

    fix : Fix randomize issue to handle customize lower and upper bound

--- randomize.cpp
+++ randomize.cpp
@@ -4,24 +4,24 @@
 using namespace std;
 using namespace chrono;
 
-long long unsigned int Random(int start, int end ) {
+long long unsigned int Random(int start, int end) {
     if(start == end){
         return start;
     }
     auto microSeconds = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
-    int random = microSeconds % end + start;
+    long long unsigned int random = microSeconds % (end - start + 1) + start;
 
     return random;
 }
 
 int main() {
     int x;
-    int start = 1, end  = 6;
+    int start , end  ;
     printf("input start range : ");
     cin >> start;
     printf("input end range   : ");
     cin >> end;
-    x = Random(start,end);
+     x = Random(start, end);
     cout << x << endl;
 
     return 0;
