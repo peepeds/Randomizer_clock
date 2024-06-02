445278e39a2a672fac599c1dea6b0443dbb7bcd3
peepeds <edbertmac@gmail.com>
2024-06-02 13:36:09 +0700

    chore : bump to 2.0.0

--- randomize.cpp
+++ randomize.cpp
@@ -4,22 +4,24 @@
 using namespace std;
 using namespace chrono;
 
-int Random(int start = 1, int end = 6) {
-    //FILE *fp ;
-    //fp = fopen("Sample.txt", "a+");
-
+long long unsigned int Random(int start, int end ) {
+    if(start == end){
+        return start;
+    }
     auto microSeconds = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
     int random = microSeconds % end + start;
-
-    //fprintf(fp, "%d\n",random);
-    //fclose(fp);
 
     return random;
 }
 
 int main() {
     int x;
-    x = Random();
+    int start = 1, end  = 6;
+    printf("input start range : ");
+    cin >> start;
+    printf("input end range   : ");
+    cin >> end;
+    x = Random(start,end);
     cout << x << endl;
 
     return 0;
